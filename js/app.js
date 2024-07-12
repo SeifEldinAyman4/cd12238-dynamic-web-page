7c49927d5e7517371d91ccc19d36741dd7ce591b
Seif Eldin Ayman <seif.eldi77@gmail.com>
2024-07-12 10:48:30 +0200

    Step 2 Completed
    Functional dynamic navbar achieved

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 /** TODO: Build the navigation bar using JavaScript **/
+let sectionList = document.getElementsByTagName("section");
+let sectionIDs = [];    
 
+for (let i = 0; i < sectionList.length; i++) {
+    sectionIDs[i] = sectionList[i].id;
+}
+
+let navbar = document.getElementById("navbar__list");
+
+for (let i = 0; i < sectionList.length; i++) {
+     let navElement = document.createElement("li");
+     let navLink = document.createElement("a");
+     navLink.textContent = `Section ${i + 1}`;
+     navLink.setAttribute("href", `#${sectionIDs[i]}`);
+     navElement.appendChild(navLink);
+     navLink.classList.add("menu__link");
+     navbar.appendChild(navElement);
+}
 /** TODO: Add smooth scrolling **/
 
 /** TODO: Add an active state **/
